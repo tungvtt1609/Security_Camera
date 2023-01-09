36065b4fe21e51654c2faf78c036c1a5224b6c03
Võ Thanh Tùng <parkhoon1609@gmail.com>
2023-01-10 01:03:05 +0700

    add function

--- face_rec.py
+++ face_rec.py
@@ -16,6 +16,9 @@ import playsound
 import wikipedia
 from gtts import gTTS
 import telepot
+import datetime
+from telepot.loop import MessageLoop
+
 
 #Initialize 'currentname' to trigger only when a new person is identified.
 currentname = "unknown"
@@ -45,111 +48,141 @@ time.sleep(2.0)
 fps = FPS().start()
 
 # Initialize sound of function
-# def play_sound(path):
-#     os.system('aplay ' + path)
-wikipedia.set_lang('vi')
-language = 'vi'
+
 
 # Cau hinh duong dan den file alarm.wav
 wav_path = "/home/pi/Security_Camera/warning.wav"
 wav_path1 = "/home/pi/Security_Camera/done.wav"
-def speak(text):
-    tts = gTTS(text=text, lang=language, slow=False)
-    tts.save("sound.mp3")
-    playsound.playsound("sound.mp3", False)
-    os.remove("sound.mp3")
+# def speak(text):
+#     tts = gTTS(text=text, lang=language, slow=False)
+#     tts.save("sound.mp3")
+#     playsound.playsound("sound.mp3", False)
+#     os.remove("sound.mp3")
 
 def playsound(path):
 	os.system('aplay ' + path)
 # loop over frames from the video file stream
-while True:
-	# grab the frame from the threaded video stream and resize it
-	# to 500px (to speedup processing)
-	frame = vs.read()
-	frame = imutils.resize(frame, width=500)
 
-	# convert the input frame from (1) BGR to grayscale (for face
-	# detection) and (2) from BGR to RGB (for face recognition)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+def handle(msg):
+	global telegramText
+	global chat_id
 
-	# detect faces in the grayscale frame
-	rects = detector.detectMultiScale(gray, scaleFactor=1.1,
-                                   minNeighbors=5, minSize=(30, 30),
-                                   flags=cv2.CASCADE_SCALE_IMAGE)
+	chat_id = msg['chat']['id']
+	telegramText = msg['text']
 
-	# OpenCV returns bounding box coordinates in (x, y, w, h) order
-	# but we need them in (top, right, bottom, left) order, so we
-	# need to do a bit of reordering
-	boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+	print('Message received from' + str(chat_id))
 
-	# compute the facial embeddings for each face bounding box
-	encodings = face_recognition.face_encodings(rgb, boxes)
-	names = []
+	if telegramText == '/start':
+		bot.sendMessage(chat_id, 'Camera hoat dong')
+	
+	while True:
+		main()
 
-	# loop over the facial embeddings
-	for encoding in encodings:
-		# attempt to match each face in the input image to our known
-		# encodings
-		matches = face_recognition.compare_faces(data["encodings"], encoding)
-		name = "Unknown"  # if face is not recognized, then print Unknown
-		#speak('Xin lỗi tôi không nhận ra bạn')
-		playsound(wav_path)
+bot = telepot.Bot("5452453117:AAGgOPQ5cK30rSSOFUo05E72Os08pLh6hok")
+bot.message_loop(handle)  
 
-		# check to see if we have found a match
-		if True in matches:
-			# find the indexes of all matched faces then initialize a
-			# dictionary to count the total number of times each face
-			# was matched
-			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-			counts = {}
+def sendNotification(name):
+	global chat_id
+	global currentname
 
-			# loop over the matched indexes and maintain a count for
-			# each recognized face face
-			for i in matchedIdxs:
-				name = data["names"][i]
-				counts[name] = counts.get(name, 0) + 1
+	if currentname != name:
+		bot.sendMessage(chat_id, 'OK')
 
-			# determine the recognized face with the largest number
-			# of votes (note: in the event of an unlikely tie Python
-			# will select first entry in the dictionary)
-			name = max(counts, key=counts.get)
 
-			#If someone in your dataset is identified, print their name on the screen
-			
-			if currentname != name:
-				currentname = name
-				playsound(wav_path1)
-				#speak('Xin chào mừng bạn'+ currentname + 'đã chấm công xong')
-				print(currentname)
+def main():
+	global chat_id
+	global currentname
+
+	while True:
+		# grab the frame from the threaded video stream and resize it
+		# to 500px (to speedup processing)
+		frame = vs.read()
+		frame = imutils.resize(frame, width=500)
+
+		# convert the input frame from (1) BGR to grayscale (for face
+		# detection) and (2) from BGR to RGB (for face recognition)
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+		# detect faces in the grayscale frame
+		rects = detector.detectMultiScale(gray, scaleFactor=1.1,
+									minNeighbors=5, minSize=(30, 30),
+									flags=cv2.CASCADE_SCALE_IMAGE)
+
+		# OpenCV returns bounding box coordinates in (x, y, w, h) order
+		# but we need them in (top, right, bottom, left) order, so we
+		# need to do a bit of reordering
+		boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+
+		# compute the facial embeddings for each face bounding box
+		encodings = face_recognition.face_encodings(rgb, boxes)
+		names = []
+
+		# loop over the facial embeddings
+		for encoding in encodings:
+			# attempt to match each face in the input image to our known
+			# encodings
+			matches = face_recognition.compare_faces(data["encodings"], encoding)
+			name = "Unknown"  # if face is not recognized, then print Unknown
+			#speak('Xin lỗi tôi không nhận ra bạn')
+			sendNotification(name)
+			playsound(wav_path)
+
+			# check to see if we have found a match
+			if True in matches:
+				# find the indexes of all matched faces then initialize a
+				# dictionary to count the total number of times each face
+				# was matched
+				matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+				counts = {}
+
+				# loop over the matched indexes and maintain a count for
+				# each recognized face face
+				for i in matchedIdxs:
+					name = data["names"][i]
+					counts[name] = counts.get(name, 0) + 1
+
+				# determine the recognized face with the largest number
+				# of votes (note: in the event of an unlikely tie Python
+				# will select first entry in the dictionary)
+				name = max(counts, key=counts.get)
+
+				#If someone in your dataset is identified, print their name on the screen
 				
+				if currentname != name:
+					currentname = name
+					playsound(wav_path1)
+					#speak('Xin chào mừng bạn'+ currentname + 'đã chấm công xong')
+					sendNotification(name)
+					print(currentname)
+					
 
-		# update the list of names
-		names.append(name)
+			# update the list of names
+			names.append(name)
 
-	# loop over the recognized faces
-	for ((top, right, bottom, left), name) in zip(boxes, names):
-		# draw the predicted face name on the image – color is in BGR
-		cv2.rectangle(frame, (left, top), (right, bottom),(0, 255, 0), 2)
-		y = top - 15 if top - 15 > 15 else top + 15
-		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,0.8, (255, 0, 0), 2)
+		# loop over the recognized faces
+		for ((top, right, bottom, left), name) in zip(boxes, names):
+			# draw the predicted face name on the image – color is in BGR
+			cv2.rectangle(frame, (left, top), (right, bottom),(0, 255, 0), 2)
+			y = top - 15 if top - 15 > 15 else top + 15
+			cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,0.8, (255, 0, 0), 2)
 
-	# display the image to our screen
-	cv2.imshow("Facial Recognition is Running", frame)
-	key = cv2.waitKey(1) & 0xFF
+		# display the image to our screen
+		cv2.imshow("Facial Recognition is Running", frame)
+		key = cv2.waitKey(1) & 0xFF
 
-	# quit when 'q' key is pressed
-	if key == ord("q"):
-		break
+		# quit when 'q' key is pressed
+		if key == ord("q"):
+			break
 
-	# update the FPS counter
-	fps.update()
+		# update the FPS counter
+		fps.update()
 
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+	# stop the timer and display FPS information
+	fps.stop()
+	print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+	print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+	# do a bit of cleanup
+	cv2.destroyAllWindows()
+	vs.stop()
